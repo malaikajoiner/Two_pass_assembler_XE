d50a190eff99da897574d257f808a062de6eeb69
Ryoka Kobayashi <rkobayashi6973@sdsu.edu>
2026-04-19 03:53:16 -0700

    pass 1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
+
+void pass1(string filename);
+//void pass2(string filename);
+
 int main(int argc, char* argv[]){
     if (argc <= 1){cout << "no file" << endl; return 0;}
     for (int i = 1; i < argc; i++){
         string filename = argv[i];
         pass1(filename);
-        pass2(filename);
+        //pass2(filename);
     }
     return 0;
 }
---
+++ pass1.cpp
@@ -0,0 +1,208 @@
+#include <map>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+
+// Format1
+bool isFormat1(string opcode) {
+    if (opcode == "FIX"  || opcode == "FLOAT" || opcode == "HIO" ||
+        opcode == "NORM" || opcode == "SIO"   || opcode == "TIO")
+        return true;
+    return false;
+}
+
+// Format2
+bool isFormat2(string opcode) {
+    if (opcode == "ADDR"  || opcode == "CLEAR" || opcode == "COMPR" ||
+        opcode == "DIVR"  || opcode == "MULR"  || opcode == "RMO"   ||
+        opcode == "SHIFTL"|| opcode == "SHIFTR"|| opcode == "SUBR"  ||
+        opcode == "SVC"   || opcode == "TIXR")
+        return true;
+    return false;
+}
+
+// Format 3
+bool isInstruction(string opcode) {
+    if (opcode == "ADD"  || opcode == "ADDF" || opcode == "AND"  ||
+        opcode == "COMP" || opcode == "COMPF"|| opcode == "DIV"  ||
+        opcode == "DIVF" || opcode == "J"    || opcode == "JEQ"  ||
+        opcode == "JGT"  || opcode == "JLT"  || opcode == "JSUB" ||
+        opcode == "LDA"  || opcode == "LDB"  || opcode == "LDCH" ||
+        opcode == "LDF"  || opcode == "LDL"  || opcode == "LDS"  ||
+        opcode == "LDT"  || opcode == "LDX"  || opcode == "MUL"  ||
+        opcode == "MULF" || opcode == "OR"   || opcode == "RD"   ||
+        opcode == "RSUB" || opcode == "STA"  || opcode == "STB"  ||
+        opcode == "STCH" || opcode == "STF"  || opcode == "STI"  ||
+        opcode == "STL"  || opcode == "STS"  || opcode == "STSW" ||
+        opcode == "STT"  || opcode == "STX"  || opcode == "SUB"  ||
+        opcode == "SUBF" || opcode == "TD"   || opcode == "TIX"  ||
+        opcode == "WD")
+        return true;
+    return false;
+}
+
+struct LineInfo {
+    int address;
+    string label;
+    string opcode;
+    string operand;
+};
+
+void pass1(string filename) {
+    ifstream file; // "file reader" used to read file
+    file.open(filename);
+    string stFile = filename.substr(0, filename.length() - 4) + ".st"; // -4 => delete .sic from the file name
+    ofstream outFile(stFile);
+
+    if (!outFile) {
+        cout << "Unable to open file for SYMTAB" << endl;
+        return;
+    }
+
+    if (!file) {
+        cout << "can't open the file" << endl;
+        return;
+    }
+
+    string line;
+    int locctr = 0;
+    int startAddress = 0;
+
+    map<string, int> SYMTAB;
+
+    vector<LineInfo> lines; // save all line info => so we can access each line later in pass 2
+
+    while (getline(file, line)) { // reading each line in the file
+        if (line.empty()) continue;
+        if (line[0] == '.') continue;
+
+        if (line[0] == '*') {
+            istringstream iss(line);
+            string star, literal;
+            iss >> star >> literal;
+
+            int currentAddress = locctr;
+
+            if (literal.size() > 3 && literal[1] == 'C') {
+                locctr += literal.length() - 3;
+            } else if (literal.size() > 3 && literal[1] == 'X') {
+                locctr += (literal.length() - 3) / 2;
+            }
+
+            LineInfo info;
+            info.address = currentAddress;
+            info.label = "*";
+            info.opcode = literal;
+            info.operand = "";
+            lines.push_back(info);
+            continue;
+        }
+
+// separate to label, opcode, and operand
+        istringstream iss(line); // making parser( reads a line and splits it into meaningful pieces)
+        string label, opcode, operand;
+
+        if (line[0] == ' ' || line[0] == '\t') { // no LABEL
+            iss >> opcode >> operand;
+            label = "";
+        } else {
+            iss >> label >> opcode >> operand;
+        }
+
+// address
+        int currentAddress;
+
+        //Originally, operand is a string, such as "0" or "1000", so we need to convert it to an integer. stoi is doing that!!
+        if (opcode == "START") {
+            startAddress = stoi(operand, nullptr, 16);
+            locctr = startAddress;
+            currentAddress = locctr;
+        } else {
+            currentAddress = locctr;
+
+            if (opcode == "WORD") {
+                locctr += 3;
+            } else if (opcode == "RESW") {
+                locctr += 3 * stoi(operand);
+            } else if (opcode == "RESB") {
+                locctr += stoi(operand);
+            } else if (opcode == "BYTE"){
+                if (!operand.empty() && operand[0] == 'C'){
+                    locctr += operand.length() - 3;
+                } else if (!operand.empty() && operand[0] == 'X'){
+                    locctr += (operand.length()- 3) /2;
+                }
+            } else if (opcode == "BASE" || opcode == "END") {
+                locctr += 0;
+            } else if (isFormat1(opcode)) {
+                locctr += 1;
+            } else if (isFormat2(opcode)) {
+                locctr += 2;
+            } else if (!opcode.empty() && opcode[0] == '='){
+                if (!operand.empty() && operand[0] == 'C'){
+                    locctr += operand.length() - 4;
+                } else if (!operand.empty() && operand[0] == 'X'){
+                    locctr += (operand.length()- 4) /2;
+                } else {
+                    cout << "Invalid literal: " << opcode << endl;
+                }
+            } else if (!opcode.empty() && opcode[0] == '+') { // if we don't put the first "empty" part, it will crash when opcode is empty.
+                locctr += 4;
+            } else if (isInstruction(opcode)) {
+                locctr += 3;
+            } else if (opcode[0] == '=') {
+                    // skip
+            } else {
+                cout << "Invalid opcode: " << opcode << endl;
+            }
+        }
+
+// SYMTAB
+        if (!label.empty() && label != "*" && label[0] != '='){
+            if (SYMTAB.find(label) != SYMTAB.end()){ //　find => iterator( the location of the label) or SYMTAB.end() (not found)
+                cout << "Duplicate label: " << label << endl;
+            } else {
+                SYMTAB[label] = currentAddress;
+            }
+        }
+
+// save the information for each line
+        LineInfo info;
+        info.address = currentAddress;
+        info.label = label;
+        info.opcode = opcode;
+        info.operand = operand;
+        lines.push_back(info);
+
+/*
+    test(address, label, opcode, operand)
+        cout << "address: " << currentAddress
+             << ", label: " << label
+             << ", opcode: " << opcode
+             << ", operand: " << operand << endl;
+*/
+    }
+
+// length
+    int programLength = locctr - startAddress;
+    cout << "Program length: " << hex << programLength << endl;
+    cout << dec;
+// open outputfile
+    if (outFile.is_open()) {
+        outFile << "SYMTAB:" << endl;
+        for (auto &pair: SYMTAB) {
+            outFile << pair.first << " -> " << hex << uppercase << pair.second << endl;
+        }
+        outFile.close();
+    } else{
+        cout << "Unable to open file for SYMTAB" << endl;
+    }
+
+/*    cout << "SYMTAB:" << endl;
+    for (auto &pair : SYMTAB) {
+        cout << pair.first << " -> " << pair.second << endl; }*/
+
+}
