fceda4cd3d3e847e700e718c724a3cbcf778baab
Malaika Joiner <malaikajoiner@gmail.com>
2026-04-21 15:58:23 -0700

    added to pass 2

--- pass2.cpp
+++ pass2.cpp
@@ -11,4 +11,40 @@ This file is the pass 2 cpp file
 
 #include "pass2.h"
 
-void pass2(string filename){}
+void pass2(string filename){
+    IF.open(filename+"_intermediate_file");     //opens an intermediate file for current input file
+    LF.open(filename+"_intermediate_file");     //opens a listing file for current input file
+    getline(IF, str_line); 
+    write(LF, ".  SOURCE CODE FOR THE XE VERSION OF THE SIC FAMILY OF COMPUTER", true);
+    objectCode = "";
+    TR_len = 0;
+    curr_record = "";
+    MR = "";
+    nobase = true;
+    block = 0;
+
+    read_IF(IF, )
+
+}
+
+string read_section(const string& section, int& index){                 //reads a section until parsed by white space
+    str_read = "";
+    
+    while(index < section.length() && isspace(section[index])){           //increments until it finds non-white space
+        index++;
+    }
+
+    while(index < section.length() && !isspace(section[index])){        //adds character to str_read until it hits a white space
+        str_read +=section[index];
+        index++;
+    }
+    return str_read;
+}
+
+void write(ofstream& filename, string text, bool nextLine){             //nextLine determines if next text begins on a new line.     
+    if(nextLine){
+        filename<<text<<endl;       
+    }else{
+        filename<<text;
+    }
+}
--- pass2.h
+++ pass2.h
@@ -12,15 +12,32 @@ This file declares the functions and constants used in pass2.cpp
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 ifstream IF;                    //reads intermediate file 
 ofstream LF, SYMTAB;            //creates output files (Listing file and Symbol Table)
 
-string str;                     //string for writing
+string str_line;                //temp string for reading whole line
+string str_read;                //temp string for reading a section
+
+string objectCode;                 
+int TR_len;                      //text record length
+string curr_record;              //current text record
+string MR;                      //Modification record
+bool nobase;
+int block = 0;
 
 void pass2(string filename){}   //main function for calling pass 2 from main.cpp
 
+void write(ofstream& filename, string text, bool nextLine){} //writes text to given file
+
+string read_section(const string& section, int& index){}
+
+bool read_IF(ifstream& filename, bool& isComment, int& lineNumber, int& address, int& block, string& label, string& opcode, string& operand, string& comment){
+    string file = "";
+    string buffer = ""
+}
 
 
 
